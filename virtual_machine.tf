d15b1c5f740371858c1a383e77501372d6d9bf04
GohilRakesh06 <160380569+GohilRakesh06@users.noreply.github.com>
2024-07-16 13:43:19 +0530

    Update virtual_machine.tf

--- virtual_machine.tf
+++ virtual_machine.tf
@@ -11,7 +11,7 @@ resource "azurerm_linux_virtual_machine" "machine1" {
 
   admin_ssh_key {
     username   = "rakeshadmin"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDUJPfw/X1CAXGAt3P1kwnZvbwkODneGqzZOQwWP6eTU/CCiXt3lKUAxuN8wVv6Da+Gkp1VMffGNbphGAzGv1n96mLIbUUhwTkAb2HSF/CtgZ3Jzq9Om6J2obNIH7NqS56TYoI5tWRs+IQxoZDxDWgL9Y04guUMEQGa+6H+pYgznufIyGlt7CR2ngHcx6K+/h0P/RBCEVufKuHviRkU1g0+8QqCRdzYzKsFewFzq+JXa75FuP7LfkYk8IWbl1eEbQti3ctBn4gqm9o26jOM1cdtz/eGWbv9U0S27tTKvklEq/dLdoMDZwpfN0H1Qu5dIC5+RMzypRnfUgypNdbChsCDv1fHsz86AVlGGTy72bQRHpWiFmw3z+A/nFL+wCbHd65rHpWa6U+Zau/CVzgaa4PZvhH4X5tLYdGcOdpYQ8h4v8+yKR9/1eNmSCWTIAmOFftidH4OmHu1Qzgssgw9BKtNyhtMxsAGQQ3JouifQlkfm0sHl0wI6p/fwZHVcbKXYUk= root@jenkinsos"
+    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE2sZrG12FpNCWHj1HxWQA0hmv+fX1OehfyOZOup/lM/ rakes@DESKTOP-GRBD6N1"
 
   }
 
