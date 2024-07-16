1a2096111633cce3eaf7d79d9671be9348e5ba9b
GohilRakesh06 <160380569+GohilRakesh06@users.noreply.github.com>
2024-07-16 13:44:29 +0530

    Update virtual_machine.tf

--- virtual_machine.tf
+++ virtual_machine.tf
@@ -11,7 +11,7 @@ resource "azurerm_linux_virtual_machine" "machine1" {
 
   admin_ssh_key {
     username   = "rakeshadmin"
-    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE2sZrG12FpNCWHj1HxWQA0hmv+fX1OehfyOZOup/lM/ rakes@DESKTOP-GRBD6N1"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCreus5i3SyEpq/QXL7Hi1BTx9WQ06cyRxxuYtMjT/Fjm73mnDiwwjQirRhSBOs6BVvPHK2ycY0Li5KEIs7xhdrYTmM15g6nCXkCvnmqPnlDZxYBr0YioKSiD2N8/XJ2HhXbsdmODyz9AbwmYKWah4a1OFqDAXdf2v4Bg9VqrerXyMNsGEh80LikkaSs59a9Aw5w0Vq+Wyy5hNDtcFmVGcsaNgwUlJRVH7c+MMI75cMaouDmBcyzFOtCzBk29y6znnh1125L0eqnOvBQfQFSy3jM3sKM68qIy7heNIlY73jfZGIygSaFCCs+0X6NdK5eub0MP6Ebc0dL2J3i0BJNweyv+zwkyPnZ/TNROcxgAbUlNORvYtE05gia25UKsznegiCvA4C/4oiBTpMipERU73W1buq1kfPcMowsnwA2pTPNpihWupE/Cqz2Moc84yIpaCJ7BMLzUfXnjyV55NNi8wkPY16/MCTzixWwe8r/hYKEupxj4+xbax60iNDg84Wdo8= rakes@DESKTOP-GRBD6N1"
 
   }
 
