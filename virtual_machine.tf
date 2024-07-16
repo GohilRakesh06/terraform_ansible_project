fe5e3675759b417030537eab60139716925cc38c
GohilRakesh06 <160380569+GohilRakesh06@users.noreply.github.com>
2024-07-16 11:53:37 +0530

    Update virtual_machine.tf

--- virtual_machine.tf
+++ virtual_machine.tf
@@ -11,7 +11,8 @@ resource "azurerm_linux_virtual_machine" "machine1" {
 
   admin_ssh_key {
     username   = "rakeshadmin"
-    public_key = azapi_resource_action.ssh_public_key_gen.output.publicKey
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDUJPfw/X1CAXGAt3P1kwnZvbwkODneGqzZOQwWP6eTU/CCiXt3lKUAxuN8wVv6Da+Gkp1VMffGNbphGAzGv1n96mLIbUUhwTkAb2HSF/CtgZ3Jzq9Om6J2obNIH7NqS56TYoI5tWRs+IQxoZDxDWgL9Y04guUMEQGa+6H+pYgznufIyGlt7CR2ngHcx6K+/h0P/RBCEVufKuHviRkU1g0+8QqCRdzYzKsFewFzq+JXa75FuP7LfkYk8IWbl1eEbQti3ctBn4gqm9o26jOM1cdtz/eGWbv9U0S27tTKvklEq/dLdoMDZwpfN0H1Qu5dIC5+RMzypRnfUgypNdbChsCDv1fHsz86AVlGGTy72bQRHpWiFmw3z+A/nFL+wCbHd65rHpWa6U+Zau/CVzgaa4PZvhH4X5tLYdGcOdpYQ8h4v8+yKR9/1eNmSCWTIAmOFftidH4OmHu1Qzgssgw9BKtNyhtMxsAGQQ3JouifQlkfm0sHl0wI6p/fwZHVcbKXYUk= root@jenkinsos"
+
   }
 
   os_disk {
