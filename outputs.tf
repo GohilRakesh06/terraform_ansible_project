d904d59049b07c3951bcd18e89b29f64146037eb
GohilRakesh06 <160380569+GohilRakesh06@users.noreply.github.com>
2024-07-17 08:32:18 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,12 @@ output "rg_name" {
   value = var.resource_group_name
 }
 
+output "vm_public_ips" {
+  value = azurerm_public_ip.vm-publicip.ip_address
+}
+
+
+
+output "vm_names" {
+  value = azurerm_linux_virtual_machine.machine1.name
+}
